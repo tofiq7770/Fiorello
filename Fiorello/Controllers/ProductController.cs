4a2fa17aa915a47a964c6db09c5ee0a61dc9636a
tofiq7770 <tofigtn@code.edu.az>
2024-05-07 20:38:15 +0400

    add

--- Fiorello/Controllers/ProductController.cs
+++ Fiorello/Controllers/ProductController.cs
@@ -14,10 +14,12 @@ namespace Fiorello.Controllers
         }
         public async Task<IActionResult> Index(int? id)
         {
-            Product product = await _context.Products.Include(m => m.Category)
-                                                      .Include(m => m.ProductImage)
-                                                      .FirstOrDefaultAsync(m => m.Id == id);
-
+            if (id is null) return BadRequest();
+            Product product = await _context.Products.Where(m => !m.SoftDelete)
+                                                     .Include(m => m.Category)
+                                                     .Include(m => m.ProductImage)
+                                                     .FirstOrDefaultAsync(m => m.Id == id);
+            if (product is null) return NotFound();
             return View(product);
         }
     }
