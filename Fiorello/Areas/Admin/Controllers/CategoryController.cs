82cd200a5965d61c9a2b571c1311651313cfd8aa
tofiq7770 <tofigtn@code.edu.az>
2024-05-12 08:23:22 +0400

    Admin update

---
+++ Fiorello/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Fiorello.DAL;
+using Fiorello.Models;
+using Fiorello.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context, ICategoryService categoryService)
+        {
+            _context = context;
+            _categoryService = categoryService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _categoryService.GetAllAsync();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+
+    }
+}
